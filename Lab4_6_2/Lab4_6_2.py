bcec291a2512df7b5279fe8ef6a323f9c2c6df6e
Rene Rotvig  Jensen <renerotvig@msn.com>
2017-04-14 09:21:21 +0200

    Seems to work now, but results are not correct

--- Lab4_6_2/Lab4_6_2.py
+++ Lab4_6_2/Lab4_6_2.py
@@ -1,27 +1,11 @@
 import pandas as pd
 import statsmodels.api as sm
-import statsmodels.formula.api as smf
 
 Smarket = pd.read_csv('Smarket.csv', index_col=0)
 
+dta = Smarket[['Lag1', 'Lag2', 'Lag3', 'Lag4', 'Lag5', 'Volume', 'Direction']]
 
-star98 = sm.datasets.star98.load_pandas().data
-formula = 'Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume'
-dta = Smarket[['NABOVE', 'NBELOW', 'LOWINC', 'PERASIAN', 'PERBLACK', 'PERHISP',
-              'PCTCHRT', 'PCTYRRND', 'PERMINTE', 'AVYRSEXP', 'AVSALK',
-              'PERSPENK', 'PTRATIO', 'PCTAF']]
-endog = dta['NABOVE'] / (dta['NABOVE'] + dta.pop('NBELOW'))
-del dta['NABOVE']
-dta['SUCCESS'] = endog
-
-'''
-import pandas as pd
-import statsmodels.api as sm
-Smarket = pd.read_csv('Smarket.csv', index_col=0)
-
-glm_model = sm.GLM(formula="Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume", data=Smarket, family=sm.families.Binomial())
-glm_results = glm_model.fit()
-
+glm_results = sm.GLM.from_formula(formula='Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume', data=dta, family=sm.families.Binomial()).fit()
 print glm_results.summary()
 
 # Parameter estimates:
@@ -29,4 +13,3 @@ print glm_results.params
 
 # The corresponding t-values:
 print glm_results.tvalues
-'''
