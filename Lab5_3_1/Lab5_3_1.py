b54f8f8639ed2dbc4d62e7771c119196283b4e8d
Rene Rotvig  Jensen <renerotvig@msn.com>
2017-05-04 13:43:09 +0200

    Lab5_3_1 added

---
+++ Lab5_3_1/Lab5_3_1.py
@@ -0,0 +1,49 @@
+from sklearn import linear_model
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing as pp
+import numpy as np
+
+def poly(x, p):
+    x = np.array(x)
+    X = np.transpose(np.vstack((x**k for k in range(p+1))))
+    return np.linalg.qr(X)[0][:,1:]
+
+data = pd.read_csv('Auto.csv', usecols=range(0,8), parse_dates=True)
+df = pd.DataFrame({'mpg': data['mpg'],'horsepower': data['horsepower'].apply(pd.to_numeric, errors='coerce')})
+train, test = train_test_split(df, test_size = 0.5)
+
+X_train = train[['horsepower']]
+Y_train = train['mpg']
+
+X_test = test[['horsepower']]
+Y_test = test['mpg']
+
+# Create linear regression object
+lm = linear_model.LinearRegression()
+lm.fit(X_train, Y_train)
+
+print("Mean squared error with out Poly: %.2f"
+      % np.mean((Y_test -lm.predict(X_test)) ** 2))
+
+# Polyomial 2-degree
+poly = pp.PolynomialFeatures(2)
+x = poly.fit_transform(X_train)
+x_t = poly.fit_transform(X_test)
+lm_poly = linear_model.LinearRegression()
+lm_poly.fit(x, Y_train)
+
+print("Mean squared error with 2-degree Poly: %.2f"
+      % np.mean((Y_test - lm_poly.predict(x_t)) ** 2))
+
+
+# Polyomial 3-degree
+poly = pp.PolynomialFeatures(3)
+x = poly.fit_transform(X_train)
+x_t = poly.fit_transform(X_test)
+lm_poly = linear_model.LinearRegression()
+lm_poly.fit(x, Y_train)
+
+print("Mean squared error with 3-degree Poly: %.2f"
+      % np.mean((Y_test - lm_poly.predict(x_t)) ** 2))
+
